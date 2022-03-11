032b07526b4e0c85d3cdfa94a9b4f85e46e23920
princeardayfio <princeardayfio@yahoo.com>
2022-03-11 02:23:31 +0000

    C program that prints verbatim

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
- *Main entry - Prints to string
- *Description: Prints "and that piece of art is useful.." without puts
- *Return: 1
-*/
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+
 
 int main(void)
 {
+
 char *s;
 
 s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
