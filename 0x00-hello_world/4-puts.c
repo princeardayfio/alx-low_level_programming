00adec92813d2c462aca53484ed3cc2ef3348d36
princeardayfio <princeardayfio@yahoo.com>
2023-02-17 03:31:45 +0000

    [File]: prints out a text followed by new line using fucntion `puts`

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - Entrypoint of code
+*
+*Return: Always 0 (success)
+*/
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
